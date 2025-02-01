3aa857dd1cc87bbab009276e4e38bf6641ac0f85
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-01 12:28:21 -0300

    creating api method

--- api.tf
+++ api.tf
@@ -1,15 +1,20 @@
-# Criar um API Gateway REST
 resource "aws_api_gateway_rest_api" "fiapx_api" {
   name        = "fiapx-api"
   description = "API Gateway protegido pelo Cognito"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
-# Criar um Authorizer no API Gateway para usar o Cognito
 resource "aws_api_gateway_authorizer" "cognito_auth" {
   name          = "cognito-authorizer"
   rest_api_id   = aws_api_gateway_rest_api.fiapx_api.id
   type          = "COGNITO_USER_POOLS"
   provider_arns = [aws_cognito_user_pool.fiapx_pool.arn]
+}
+
+resource "aws_api_gateway_deployment" "fiapx_api" {
+  rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
 }
 
 resource "aws_api_gateway_stage" "fiapx_api_stage" {
@@ -18,6 +23,33 @@ resource "aws_api_gateway_stage" "fiapx_api_stage" {
   deployment_id = aws_api_gateway_deployment.fiapx_api.id
 }
 
-resource "aws_api_gateway_deployment" "fiapx_api" {
+resource "aws_api_gateway_resource" "fiapx_api_resource" {
   rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
+  parent_id   = aws_api_gateway_rest_api.fiapx_api.root_resource_id
+  path_part   = "videos"
+}
+
+resource "aws_api_gateway_method" "api_gateway_method_get" {
+  rest_api_id   = aws_api_gateway_rest_api.fiapx_api.id
+  resource_id   = aws_api_gateway_resource.fiapx_api.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "api_gateway_method_post" {
+  rest_api_id   = aws_api_gateway_rest_api.fiapx_api.id
+  resource_id   = aws_api_gateway_resource.fiapx_api.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_settings" "s" {
+  rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
+  stage_name  = aws_api_gateway_stage.fiapx_api.stage_name
+  method_path = "${aws_api_gateway_resource.fiapx_api.path_part}/${aws_api_gateway_method.fiapx_api.http_method}"
+
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
 }
