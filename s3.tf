93dfe4adfe787491b0bc8098bb6d3c8102d6552a
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-01 11:09:17 -0300

    trying create bucket

--- s3.tf
+++ s3.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state"
-  acl    = "private"
+  bucket        = "terraform-state"
+  acl           = "private"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
