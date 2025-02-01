cca7db5a128e75194cc12fbc0e464c1b9aae2dac
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-01 11:13:16 -0300

    using already bucket created

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state"
-    key            = "terraform.tfstate"
+    bucket         = "fiap-x-storage"
+    key            = "terraform/state/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
