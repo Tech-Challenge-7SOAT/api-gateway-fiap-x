14eb4d3c77bf031c108031dbabe59a0fb4d924e0
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-01 16:04:12 -0300

    fixing policy

--- cloudwatch-policy.tf
+++ cloudwatch-policy.tf
@@ -1,9 +1,23 @@
+resource "aws_iam_role" "apigateway_logging_role" {
+  name = "APIGatewayCloudWatchRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      }
+    }]
+  })
+}
 resource "aws_iam_policy_attachment" "apigateway_logging_attach" {
   name       = "APIGatewayLoggingAttachment"
-  roles      = var.labRole
+  roles      = [aws_iam_role.apigateway_logging_role.name]
   policy_arn = var.policyArn
 }
 
 resource "aws_api_gateway_account" "apigateway_logging" {
-  cloudwatch_role_arn = var.labRole
+  cloudwatch_role_arn = aws_iam_role.apigateway_logging_role.arn
 }
