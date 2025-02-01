53581042cab47d29d0bf187c89c520184f59148d
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-01 15:36:08 -0300

    deploying api final

--- api.tf
+++ api.tf
@@ -13,12 +13,6 @@ resource "aws_api_gateway_authorizer" "cognito_auth" {
   provider_arns = [aws_cognito_user_pool.fiapx_pool.arn]
 }
 
-resource "aws_api_gateway_stage" "fiapx_api_stage" {
-  stage_name    = "prod"
-  rest_api_id   = aws_api_gateway_rest_api.fiapx_api.id
-  deployment_id = aws_api_gateway_deployment.fiapx_api.id
-}
-
 resource "aws_api_gateway_resource" "fiapx_api" {
   rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
   parent_id   = aws_api_gateway_rest_api.fiapx_api.root_resource_id
@@ -59,31 +53,4 @@ resource "aws_api_gateway_method_settings" "s_post" {
     metrics_enabled = true
     logging_level   = "INFO"
   }
-}
-
-resource "aws_api_gateway_integration" "get_integration" {
-  rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
-  resource_id = aws_api_gateway_resource.fiapx_api.id
-  http_method = aws_api_gateway_method.api_gateway_method_get.http_method
-  type        = "HTTP_PROXY"
-  integration_http_method = "GET"
-  uri         = "https://ecs-alb-940467805.us-east-1.elb.amazonaws.com"
-}
-
-resource "aws_api_gateway_integration" "post_integration" {
-  rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
-  resource_id = aws_api_gateway_resource.fiapx_api.id
-  http_method = aws_api_gateway_method.api_gateway_method_post.http_method
-  type        = "HTTP_PROXY"
-  integration_http_method = "GET"
-  uri         = "https://ecs-alb-940467805.us-east-1.elb.amazonaws.com"
-}
-
-resource "aws_api_gateway_deployment" "fiapx_api" {
-  depends_on = [
-    aws_api_gateway_integration.get_integration,
-    aws_api_gateway_integration.post_integration
-  ]
-  rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
-  stage_name  = "prod"
 }
