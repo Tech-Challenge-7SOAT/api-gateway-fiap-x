808ef90825842de441c085eda3da78bf94da0279
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-01 15:13:16 -0300

    fixing aws_api_gateway_method_settings

--- api.tf
+++ api.tf
@@ -13,17 +13,17 @@ resource "aws_api_gateway_authorizer" "cognito_auth" {
   provider_arns = [aws_cognito_user_pool.fiapx_pool.arn]
 }
 
-resource "aws_api_gateway_deployment" "fiapx_api_deployment" {
+resource "aws_api_gateway_deployment" "fiapx_api" {
   rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
 }
 
 resource "aws_api_gateway_stage" "fiapx_api_stage" {
   stage_name    = "prod"
   rest_api_id   = aws_api_gateway_rest_api.fiapx_api.id
-  deployment_id = aws_api_gateway_deployment.fiapx_api_deployment.id
+  deployment_id = aws_api_gateway_deployment.fiapx_api.id
 }
 
-resource "aws_api_gateway_resource" "fiapx_api_resource" {
+resource "aws_api_gateway_resource" "fiapx_api" {
   rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
   parent_id   = aws_api_gateway_rest_api.fiapx_api.root_resource_id
   path_part   = "videos"
@@ -31,22 +31,33 @@ resource "aws_api_gateway_resource" "fiapx_api_resource" {
 
 resource "aws_api_gateway_method" "api_gateway_method_get" {
   rest_api_id   = aws_api_gateway_rest_api.fiapx_api.id
-  resource_id   = aws_api_gateway_resource.fiapx_api_resource.id
+  resource_id   = aws_api_gateway_resource.fiapx_api.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_method" "api_gateway_method_post" {
   rest_api_id   = aws_api_gateway_rest_api.fiapx_api.id
-  resource_id   = aws_api_gateway_resource.fiapx_api_resource.id
+  resource_id   = aws_api_gateway_resource.fiapx_api.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_settings" "s" {
+resource "aws_api_gateway_method_settings" "s_get" {
   rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
   stage_name  = aws_api_gateway_stage.fiapx_api_stage.stage_name
-  method_path = ["${aws_api_gateway_resource.fiapx_api_resource.path_part}/${api_gateway_method_get.fiapx_api.http_method}","${aws_api_gateway_resource.fiapx_api_resource.path_part}/${api_gateway_method_post.fiapx_api.http_method}"]
+  method_path = "${aws_api_gateway_resource.fiapx_api.path_part}/${aws_api_gateway_method.api_gateway_method_get.http_method}"
+
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
+}
+
+resource "aws_api_gateway_method_settings" "s_post" {
+  rest_api_id = aws_api_gateway_rest_api.fiapx_api.id
+  stage_name  = aws_api_gateway_stage.fiapx_api_stage.stage_name
+  method_path = "${aws_api_gateway_resource.fiapx_api.path_part}/${aws_api_gateway_method.api_gateway_method_post.http_method}"
 
   settings {
     metrics_enabled = true
