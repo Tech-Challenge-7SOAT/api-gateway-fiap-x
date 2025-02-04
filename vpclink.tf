ca0fb57e4b1141da707d4ab7a63bd3d81d809311
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-03 23:44:17 -0300

    fixing vpc link integration

--- vpclink.tf
+++ vpclink.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_vpc_link" "fiapx_vpc_link" {
   name        = "fiapx-vpc-link"
-  target_arns = [aws_lb.my_alb.arn]
+  target_arns = [aws_lb.ecs_alb.arn]
 
   lifecycle {
     create_before_destroy = true
