c53f8e521a6819d0f8d0d0ba93c5086a1face2c5
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-09 00:16:17 -0300

    fixing alb target group port

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "fiap-x-storage"
-    key            = "terraform/state/terraform.tfstate"
+    bucket         = "terraform-deploy-fiapx"
+    key            = "apigateway/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
